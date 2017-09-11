6b017acb823d17fc02956ae910bf9efe568f3ee6
Stanley Ung <StanleyUng167@gmail.com>
2017-09-11 13:49:05 -0700

    Part 2 problem 4 done

--- Project 1/Problems/Project1Problem4.py
+++ Project 1/Problems/Project1Problem4.py
@@ -1,6 +1,7 @@
 import numpy as np
 import string
 
+'''
 def generatePassword():
     password = ''
     for i in range(0, 4):
@@ -8,18 +9,37 @@ def generatePassword():
     return password
 
 def generateList():
-    N = 10**5
+    N = 10**2
     passwords = []
     for i in range (0, N):
         passwords.append(generatePassword())
     return passwords
 
-N = 10
+for i in range(0, N):
+    hackerlist = generateList()
+    if (yourPassword in hackerlist):
+        success = success + 1
+    
+p = success / N
+print('The probability of a hacker having your password is ' + str(p))
+
+'''
+possiblePasswords = 26**4
+
+# The number way
+def generatePassword():
+    return np.random.randint(0, possiblePasswords)
+
+def generateList(x):
+    return np.random.randint(0, possiblePasswords, x)
+
+# # of trials
+N = 1000
 yourPassword = generatePassword()
 success = 0
 
 for i in range(0, N):
-    hackerlist = generateList()
+    hackerlist = generateList(10**6)
     if (yourPassword in hackerlist):
         success = success + 1
     
