312f941479153a3b24201577876ac9a5ffd418e1
Stanley Ung <StanleyUng167@gmail.com>
2017-10-30 12:11:01 -0700

    Project 4 things

--- project2/Problems/Project2Problem1.py
+++
@@ -1,7 +0,0 @@
-import random
-
-p_0 = 0.5
-p_1 = 1 - p_0
-
-
-
--- project4/project4.py
+++ project4/project4.py
@@ -8,22 +8,85 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-def problem_1(n):
+def problem_1(nbooks):
+	a = 1; b = 3 # a=min bookwidth ; b=max bookwidth
+	#nbooks = 5 # Number of books
+	nbins = 30 # Number of bins
+	edgecolor = 'w' # Color separating bars in the bargraph
 	N = 10000
 
-	mu, sigma = 100, 15
-	x = [sum(np.random.uniform(1, 3, 2)) for i in range(0, N)]
+	# X is the array with the values of the RV to be plotted
+	X = [sum(np.random.uniform(a, b, nbooks)) for i in range(0, N)]
+	
+	# Create bins and histogram
+	bins = [float(x) for x in np.linspace(nbooks * a, nbooks * b,nbins + 1)]
+	h1, bin_edges = np.histogram(X, bins, density=True)
+	# Define points on the horizontal axis
+	be1 = bin_edges[0 : len(bin_edges) - 1]
+	be2 = bin_edges[1 : len(bin_edges)]
+	b1 = (be1 + be2) / 2
+	barwidth = b1[1] - b1[0] # Width of bars in the bargraph
 
-	# the histogram of the data
-	n, bins, patches = plt.hist(x, bins = 20, normed=1, facecolor='yellow', alpha=0.75)
-	# add a 'best fit' line
-	y = mlab.normpdf(bins, mu, sigma)
-	l = plt.plot(bins, y, 'r--', linewidth=1)
-	plt.xlabel('Book stack height for n = 2 books')
+	fig1 = plt.figure(1)
+
+	# Labels
+	plt.xlabel('Book stack height for n = ' + str(nbooks) + ' books')
 	plt.ylabel('PDF')
 	plt.title('PDF of book stack height and comparison with Gaussian')
-	plt.axis([0, 8, 0, 0.30])
-	plt.grid(True)
+	plt.bar(b1, h1, width = barwidth, edgecolor = edgecolor)
 	plt.show()
 
-problem_1(10000)
+def problem_2():
+	N = 10000
+	beta = 0.5
+
+	nbins = 30 # Number of bins
+	edgecolor = 'w' # Color separating bars in the bargraph
+
+	X = np.random.exponential(beta, N)
+
+	plt.hist(X, bins = 30)
+	plt.xlabel('')
+	plt.ylabel('')
+	plt.title('')
+	plt.bar(1, 10, width = 1, edgecolor = edgecolor)
+	plt.show()
+
+def problem_3():
+	a = 1; b = 3 # a=min bookwidth ; b=max bookwidth
+	#nbooks = 5 # Number of books
+	nbins = 30 # Number of bins
+	edgecolor = 'w' # Color separating bars in the bargraph
+	N = 10000
+	beta = 45
+
+	X = np.random.exponential(beta, N)
+	
+	# Create bins and histogram
+	bins = [float(x) for x in np.linspace(24 * a, 24 * b,nbins + 1)]
+	h1, bin_edges = np.histogram(X, bins, density=True)
+	# Define points on the horizontal axis
+	be1 = bin_edges[0 : len(bin_edges) - 1]
+	be2 = bin_edges[1 : len(bin_edges)]
+	b1 = (be1 + be2) / 2
+	barwidth = b1[1] - b1[0] # Width of bars in the bargraph
+	fig1 = plt.figure(1)
+	# Labels
+	plt.xlabel('Book stack height for n = ' + str(nbooks) + ' books')
+	plt.ylabel('PDF')
+	plt.title('PDF of book stack height and comparison with Gaussian')
+	plt.bar(b1, h1, width = barwidth, edgecolor = edgecolor)
+	plt.show()
+
+'''
+#3 is literally #1
+24 of soemthing, #1 books are uniform,
+#3 is not uniform, so use exponential distribution
+replace 15 books with 24
+instead of adding 15, now add 24
+'''
+
+
+#problem_1(1)
+problem_2()
+#problem_3()
